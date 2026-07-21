601da9e9f5434d25b359f5000e550657b943725e
shakti kumar sah <shaktisah871@gmail.com>
2026-07-21 11:48:54 +0530

    Fix missing imports and routes in App.jsx to resolve runtime blank page error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Homepage from "./pages/Homepage";
+import AdminPanel from "./pages/AdminPanel";
+import ProblemWorkspace from "./pages/ProblemWorkspace";
+import Profile from "./pages/Profile";
 import { checkAuth } from "./authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 function App() {
-  // user is authenticated
   const { isAuthenticated } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
@@ -32,11 +34,22 @@ function App() {
         element={isAuthenticated ? <Navigate to="/" /> : <Signup />}
       />
 
-
-       <Route
+      <Route
         path="/admin"
-        element={isAuthenticated ? <Navigate to="/" /> : <AdminPanel/>}
+        element={isAuthenticated ? <AdminPanel /> : <Navigate to="/login" />}
       />
+
+      <Route
+        path="/problem/:id"
+        element={isAuthenticated ? <ProblemWorkspace /> : <Navigate to="/login" />}
+      />
+
+      <Route
+        path="/profile"
+        element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+      />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
